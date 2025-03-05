13d67d781e14ab2a30905260fc92e27d09899f4b
Vinay4562 <manikantivinaykumar@yahoo.com>
2025-03-05 16:11:18 +0530

    Initial commit

--- server.js
+++ server.js
@@ -18,13 +18,11 @@ const port = process.env.PORT || 3500;
 const mongoUri = process.env.MONGO_URI;
 
 // Connect to MongoDB
-mongoose.connect(mongoUri, {
+mongoose.connect(process.env.MONGO_URI, {
     useNewUrlParser: true,
     useUnifiedTopology: true
-});
-const db = mongoose.connection;
-db.on('error', console.error.bind(console, 'MongoDB connection error:'));
-db.once('open', () => console.log('Connected to MongoDB'));
+}).then(() => console.log("✅ Connected to MongoDB"))
+  .catch(err => console.error("❌ MongoDB Connection Error:", err));
 
 // Middleware
 app.use(bodyParser.urlencoded({ extended: true }));
